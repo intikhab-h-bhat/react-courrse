9b9293d111ce05e9dd645e52d0ced8392f51b50d
intikhab-h-bhat <44907729+intikhab-h-bhat@users.noreply.github.com>
2024-09-04 18:45:37 +0530

    added components (#2)
    * intial comment
    
    * intial comment created todapp
    
    * added components ane create todo list uI

---
+++ todo-app/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+//import ReactDOM from 'react-dom/client'
+
+   
+function Header(){
+
+    return <h1 className='todo-header'>ToDO List</h1>
+}
+
+export default Header;
